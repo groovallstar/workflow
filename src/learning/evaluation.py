7687320f7d72e0ed53268861f292bada21d0d527
groovallstar <groovallstar@users.noreply.github.com>
2024-08-07 10:54:54 +0900

    modify commets

--- src/learning/evaluation.py
+++ src/learning/evaluation.py
@@ -56,22 +56,14 @@ def get_evaluation_metric(y_test, predict) -> MetricData:
         def roc_auc_score_multiclass(
             actual_class, pred_class, average = "macro") -> dict:
             """get roc_auc_score for multiclass"""
-            # creating a set of all the unique classes
-            # using the actual class list
             unique_class = set(actual_class)
             roc_auc_dict = {}
             for per_class in unique_class:
-                #creating a list of all the classes except the current class
                 other_class = [x for x in unique_class if x != per_class]
-
-                #marking the current class as 1 and all other classes as 0
                 new_actual_class = [0 if x in other_class \
                                     else 1 for x in actual_class]
                 new_pred_class = [0 if x in other_class \
                                   else 1 for x in pred_class]
-
-                # using the sklearn metrics method 
-                # to calculate the roc_auc_score
                 roc_auc = roc_auc_score(
                     new_actual_class, new_pred_class, average=average)
                 roc_auc_dict[per_class] = roc_auc
@@ -191,7 +183,7 @@ def find_best_threshold(
 
     if score_list.size:
         max_score_index = np.argmax(score_list)
-        # max_score_index 가 0일 경우는 floating_porint 에 곱할수 없으므로
+        # max_score_index 가 0일 경우는 floating_point 에 곱할수 없으므로
         # 첫번째 인덱스로 대체함
         threshold = max_score_index * floating_point\
             if (max_score_index) else (1 * floating_point)
